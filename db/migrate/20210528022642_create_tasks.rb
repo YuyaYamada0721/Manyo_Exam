8a25e131114ecff79613218761c1e2c1449869d2
yuya yamada <yuya19920721@gmail.com>
2021-05-28 14:03:58 +0900

    task_content追加のためマイグレーションファイル修正

--- db/migrate/20210528022642_create_tasks.rb
+++ db/migrate/20210528022642_create_tasks.rb
@@ -2,7 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :task_name
-
+      t.text :task_content
       t.timestamps
     end
   end
--- db/migrate/20210528034725_change_not_null_to_tasks.rb
+++ db/migrate/20210528034725_change_not_null_to_tasks.rb
@@ -1,5 +1,6 @@
 class ChangeNotNullToTasks < ActiveRecord::Migration[5.2]
   def change
     change_column_null :tasks, :task_name, false
+    change_column_null :tasks, :task_content, false
   end
 end
