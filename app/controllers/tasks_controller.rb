f70466f5c0f9782765185c75510f16a1a9a080af
yuya yamada <yuya19920721@gmail.com>
2021-05-28 17:37:08 +0900

    taskの新規作成と編集画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[show]
+  before_action :set_task, only: %i[show edit update]
 
   def index
     @tasks = Task.all
@@ -9,14 +9,35 @@ class TasksController < ApplicationController
   def show; end
 
   def new
+    @task = Task.new
   end
 
-  def edit
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to tasks_path, notice: 'タスクを登録しました。'
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: 'タスクを編集しました。'
+    else
+      render :edit
+    end
   end
 
   private
 
   def set_task
     @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:task_name, :task_content)
   end
 end
