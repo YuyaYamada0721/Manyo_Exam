ff09358b4f96b4dddc905c11d3fe33d53e08fda1
yuya yamada <yuya19920721@gmail.com>
2021-05-28 13:43:20 +0900

    rails db:migrate:redo 実施 task_contentカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_022642) do
+ActiveRecord::Schema.define(version: 2021_05_28_043232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "tasks", force: :cascade do |t|
-    t.string "task_name"
+    t.string "task_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "task_content"
   end
 
 end
