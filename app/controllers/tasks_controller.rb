1b5bb5b5823da185d9c2a8e06bc613ec6fb73ffe
yuya yamada <yuya19920721@gmail.com>
2021-05-28 16:25:33 +0900

    taskの一覧画面と詳細画面の作成

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,13 +1,22 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: %i[show]
+
   def index
+    @tasks = Task.all
+    @task = Task.new
   end
 
-  def show
-  end
+  def show; end
 
   def new
   end
 
   def edit
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
