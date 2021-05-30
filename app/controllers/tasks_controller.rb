929d2e991722190248686c54c02301bb520c2f2d
yuya yamada <yuya19920721@gmail.com>
2021-05-30 22:15:12 +0900

    タスク一覧を作成日時の順で並べる、SystemSpecテスト-複数のタスクを作成した場合、タスクが作成日時の降順に並んでいるか

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,8 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
 
   def index
-    @tasks = Task.all
-    @task = Task.new
+    @tasks = Task.all.order(created_at: :desc)
   end
 
   def show; end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -3,4 +3,14 @@ FactoryBot.define do
     task_name { 'test_name' }
     task_content { 'test_content' }
   end
+  factory :task2, class: Task do
+    task_name { 'test_name2' }
+    task_content { 'test_content2' }
+    created_at { Time.current + 1.days }
+  end
+  factory :task3, class: Task do
+    task_name { 'test_name3' }
+    task_content { 'test_content3' }
+    created_at { Time.current + 2.days }
+  end
 end
