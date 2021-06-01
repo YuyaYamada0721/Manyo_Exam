7c1d177f659b27b7d7e8eccb5ddd9c4820b29050
yuya yamada <yuya19920721@gmail.com>
2021-06-02 00:13:11 +0900

    データベースに終了期限のカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_28_034725) do
+ActiveRecord::Schema.define(version: 2021_06_01_140227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_05_28_034725) do
     t.text "task_content", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "expiration_deadline", default: -> { "now()" }, null: false
   end
 
 end
