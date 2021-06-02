51b92924ec0c7644268e57ab3df7940572b20402
yuya yamada <yuya19920721@gmail.com>
2021-06-02 13:40:09 +0900

    終了期限に関してのRSpec

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -2,15 +2,18 @@ FactoryBot.define do
   factory :task do
     task_name { 'test_name' }
     task_content { 'test_content' }
+    expiration_deadline { Time.current }
   end
   factory :task2, class: Task do
     task_name { 'test_name2' }
     task_content { 'test_content2' }
     created_at { Time.current + 1.days }
+    expiration_deadline { Time.current + 1.day }
   end
   factory :task3, class: Task do
     task_name { 'test_name3' }
     task_content { 'test_content3' }
     created_at { Time.current + 2.days }
+    expiration_deadline { Time.current + 2.day }
   end
 end
--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 RSpec.describe 'タスク管理機能', type: :system do
   let!(:task) { FactoryBot.create(:task) }
+  let!(:task3) { FactoryBot.create(:task3) }
+  let!(:task2) { FactoryBot.create(:task2) }
   before do
     visit tasks_path
   end
@@ -10,9 +12,12 @@ RSpec.describe 'タスク管理機能', type: :system do
         visit new_task_path
         fill_in 'task[task_name]', with: 'あああ'
         fill_in 'task[task_content]', with: 'いいい'
+        fill_in 'task[expiration_deadline]', with: '002021/06/03'
         click_on 'タスク登録'
+        click_on '一覧画面へ戻る'
         expect(page).to have_content 'あああ'
         expect(page).to have_content 'いいい'
+        expect(page).to have_content '2021-06-03'
       end
     end
   end
@@ -24,9 +29,6 @@ RSpec.describe 'タスク管理機能', type: :system do
     end
     context 'タスクが作成日時の降順に並んでいる場合' do
       it '新しいタスクが一番上に表示される' do
-        FactoryBot.create(:task2)
-        FactoryBot.create(:task3)
-        visit tasks_path
         task = all('.task_row')
         expect(task[0]).to have_content 'test_name3'
       end
@@ -35,8 +37,19 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
     context '任意のタスク詳細画面に遷移した場合' do
       it '該当タスクの内容が表示される' do
-        click_on '詳細'
+        first(:link, '詳細').click
         expect(page).to have_content 'test_name'
+      end
+    end
+  end
+  describe '終了期限降順機能' do
+    context '終了期限でソートした場合' do
+      it '終了期限が降順で表示される' do
+        click_on '終了期限でソートする'
+        task = all('.task_row')
+        expect(task[0]).to have_content 'test_name3'
+        expect(task[1]).to have_content 'test_name2'
+        expect(task[2]).to have_content 'test_name'
       end
     end
   end
