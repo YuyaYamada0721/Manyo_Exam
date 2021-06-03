30d91bd92ff9697cde759ca79203d91142287295
yuya yamada <yuya19920721@gmail.com>
2021-06-03 17:26:40 +0900

    優先順位のカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_054823) do
+ActiveRecord::Schema.define(version: 2021_06_03_082433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_06_03_054823) do
     t.datetime "updated_at", null: false
     t.date "expiration_deadline", default: -> { "now()" }, null: false
     t.integer "status", default: 0, null: false
+    t.integer "priority", default: 0, null: false
     t.index ["task_name", "status"], name: "index_tasks_on_task_name_and_status"
   end
 
