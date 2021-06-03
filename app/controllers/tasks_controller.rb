50042ebf0b7d2b1013221748e7ed0310b5449f76
yuya yamada <yuya19920721@gmail.com>
2021-06-03 13:28:57 +0900

    タイトル検索とステータス検索機能追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,13 +2,21 @@ class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
 
   def index
-    if params[:sort_expired]
-      @tasks = Task.order(expiration_deadline: :desc)
-    elsif params[:task_name]
-      @tasks = Task.where('task_name LIKE ?', "%#{params[:task_name]}%")
-    else
-      @tasks = Task.all.order(created_at: :desc)
-    end
+      if params[:task].present?
+        if params[:task][:task_name].present? && params[:task][:status].present?
+          @tasks = Task.where('task_name LIKE ?', "%#{params[:task][:task_name]}%").where(status: params[:task][:status])
+        elsif params[:task][:task_name].present?
+          @tasks = Task.where('task_name LIKE ?', "%#{params[:task][:task_name]}%")
+        elsif params[:task][:status].present?
+          @tasks = Task.where(status: params[:task][:status])
+        else
+          @tasks = Task.all
+        end
+      elsif params[:sort_expired]
+          @tasks = Task.order(expiration_deadline: :desc)
+      else
+          @tasks = Task.all.order(created_at: :desc)
+      end
   end
 
   def show; end
@@ -48,6 +56,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:task_name, :task_content, :expiration_deadline, :status, :task)
+    params.require(:task).permit(:task_name, :task_content, :expiration_deadline, :status, :task).merge(status: params[:task][:status].to_i)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,10 @@
 class Task < ApplicationRecord
   validates :task_name, presence: true, length: { maximum: 100 }
   validates :task_content, presence: true
+
+  enum status: {
+    未着手: 0,
+    着手中: 1,
+    完了: 2
+  }
 end
