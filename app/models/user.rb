f3fa18104e633b26d115472466b992d02c3ffdab
yuya yamada <yuya19920721@gmail.com>
2021-06-05 19:19:40 +0900

    user_emailにユニーク制約をつける

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :user_name, presence: true, length: { maximum: 30 }
-  validates :user_email, presence: true, length: { maximum: 255 },
+  validates :user_email, presence: true, length: { maximum: 255 }, uniqueness: true,
                         format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  before_validation { user_email.downcase! }
 end
