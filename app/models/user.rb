8914b8a2eb4bb2e867e00d7c8591a0187a62d42d
yuya yamada <yuya19920721@gmail.com>
2021-06-05 19:49:16 +0900

    セキュアパスワード設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   validates :user_email, presence: true, length: { maximum: 255 }, uniqueness: true,
                         format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   before_validation { user_email.downcase! }
+  validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
   has_many :tasks
 end
