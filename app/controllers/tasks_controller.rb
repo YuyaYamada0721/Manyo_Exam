887dbd40e22e67dec3c23c006ea74481313dbe79
yuya yamada <yuya19920721@gmail.com>
2021-06-06 12:44:59 +0900

    ユーザ登録した時に、同時にログインもする実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   before_action :set_task, only: %i[show edit update destroy]
+  before_action :authenticate_user, only: %i[edit update destroy]
 
   def index
     if params[:task].present?
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  skip_before_action :login_required, only: %i[new create]
+  skip_before_action :login_required, only: %i[new create show]
+  before_action :ensure_current_user, only: %i[edit update show]
+
   def new
     @user = User.new
   end
@@ -7,6 +9,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to user_path(@user.id)
     else
       render :new
@@ -20,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:user_name, :user_email, :password, :password_confirmation )
+    params.require(:user).permit(:user_name, :user_email, :password, :password_confirmation)
   end
 end
