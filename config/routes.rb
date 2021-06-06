b4aaf64917e68d664aff59758809850fedb52225
yuya yamada <yuya19920721@gmail.com>
2021-06-06 14:05:55 +0900

    管理画面のroutesとカラムを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'sessions/new'
   root to: 'tasks#index'
+  
   resources :tasks
   resources :users, only: %i[new create show]
   resources :sessions, only: %i[new create destroy]
+
+  namespace :admin do
+    resources :users
+  end
 end
