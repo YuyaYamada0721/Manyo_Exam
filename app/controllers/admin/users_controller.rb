b966289cf2bc6669878912733e940ca3cdee5e5a
yuya yamada <yuya19920721@gmail.com>
2021-06-06 18:49:18 +0900

    admin画面に新規登録実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -5,6 +5,19 @@ class Admin::UsersController < ApplicationController
     @users = User.all.includes(:tasks)
   end
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
+    else
+      render :new
+    end
+  end
+
   def edit
   end
 
