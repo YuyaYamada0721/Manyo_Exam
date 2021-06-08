96e2f92427b9da66acd45eaf1a0f9f45ca2662a6
yuya yamada <yuya19920721@gmail.com>
2021-06-09 00:49:43 +0900

    修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,11 @@
-users = [
-  { user_name: 'yamada', user_email: 'yamada@yamada.com', password: 'yamada', admin: 'true' },
-  { user_name: 'yamada2', user_email: 'yamada2@yamada.com', password: 'yamada2', admin: 'true' }
-]
-User.create! users
+User.create!(
+  [
+  { user_name: 'yamada', user_email: 'yamada@yamada.com', password: 'yamada', password_confirmation: 'yamada', admin: true },
+  { user_name: 'yamada2', user_email: 'yamada2@yamada.com', password: 'yamada2', password_confirmation: 'yamada2', admin: true }
+  ]
+)
 
-10.times do |n|
+8.times do |n|
   name = Faker::Games::Pokemon.name
   email = Faker::Internet.email
   password = "password"
@@ -12,22 +13,23 @@ User.create! users
     user_name: name,
     user_email: email,
     password: password,
+    password_confirmation: password,
     admin: false
   )
 end
 
 Label.create!(
   [
-  { label_name: 'AA' },
-  { label_name: 'BB' },
-  { label_name: 'CC' },
-  { label_name: 'DD' },
-  { label_name: 'EE' },
-  { label_name: 'FF' },
-  { label_name: 'GG' },
-  { label_name: 'HH' },
-  { label_name: 'II' },
-  { label_name: 'JJ' }
+  { label_name: "Ruby" },
+  { label_name: "Rails" },
+  { label_name: "Python" },
+  { label_name: "Java" },
+  { label_name: "JavaScript" },
+  { label_name: "PHP" },
+  { label_name: "HTML" },
+  { label_name: "CSS" },
+  { label_name: "VBS" },
+  { label_name: "GGG" }
   ]
 )
 
