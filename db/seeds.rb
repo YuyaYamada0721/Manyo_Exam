0b2b8b48d2ac7200d82aed5918d0e5ec31c37694
yuya yamada <yuya19920721@gmail.com>
2021-06-09 00:03:43 +0900

    seedデータ反映しないため修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,32 +13,32 @@ User.create! users
   )
 end
 
-Label.create!(
-  [
-  { label_name: 'A' },
-  { label_name: 'B' },
-  { label_name: 'C' },
-  { label_name: 'D' },
-  { label_name: 'E' },
-  { label_name: 'F' },
-  { label_name: 'G' },
-  { label_name: 'H' },
-  { label_name: 'I' },
-  { label_name: 'J' }
-  ]
-)
-
 Task.create!(
   [
     { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 1.day, status: 1, priority: 1, user_id: 1},
-    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 2.day, status: 1, priority: 1, user_id: 2},
-    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 3.day, status: 1, priority: 1, user_id: 3},
-    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 4.day, status: 1, priority: 1, user_id: 4},
-    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 5.day, status: 1, priority: 1, user_id: 5},
-    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 6.day, status: 1, priority: 1, user_id: 6},
-    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 7.day, status: 1, priority: 1, user_id: 7},
-    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 8.day, status: 1, priority: 1, user_id: 8},
-    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 9.day, status: 1, priority: 1, user_id: 9},
-    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 10.day, status: 1, priority: 1, user_id:101},
+    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 2.day, status: 1, priority: 1, user_id: 1},
+    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 3.day, status: 1, priority: 1, user_id: 1},
+    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 4.day, status: 1, priority: 1, user_id: 2},
+    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 5.day, status: 1, priority: 1, user_id: 2},
+    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 6.day, status: 1, priority: 1, user_id: 2},
+    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 7.day, status: 1, priority: 1, user_id: 3},
+    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 8.day, status: 1, priority: 1, user_id: 3},
+    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 9.day, status: 1, priority: 1, user_id: 3},
+    { task_name: 'test', task_content: 'test', expiration_deadline: Time.current + 10.day, status: 1, priority: 1, user_id: 4},
   ]
   )
+
+Label.create!(
+  [
+  { label_name: 'AA' },
+  { label_name: 'BB' },
+  { label_name: 'CC' },
+  { label_name: 'DD' },
+  { label_name: 'EE' },
+  { label_name: 'FF' },
+  { label_name: 'GG' },
+  { label_name: 'HH' },
+  { label_name: 'II' },
+  { label_name: 'JJ' }
+  ]
+)
