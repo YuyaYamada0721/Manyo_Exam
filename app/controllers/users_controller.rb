771ec950cb20015775ca1371adbac40e5a6ad06b
yuya yamada <yuya19920721@gmail.com>
2021-09-03 16:23:47 +0900

    アラートでお知らせするように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,9 +18,16 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @tasks = @user.tasks
     @near_the_end_task = @user.tasks.where(expiration_deadline: Time.zone.today..Time.zone.today + 3)
     @expired_task = @user.tasks.where('expiration_deadline < ?', Time.zone.today)
+
+    if @near_the_end_task.present? && @expired_task.present?
+      flash[:alert] = '終了間近のタスクと期限切れのタスクがあります！'
+    elsif @near_the_end_task.present?
+      flash[:alert] = '終了間近のタスクがあります！'
+    elsif @expired_task.present?
+       flash[:alert] = '期限切れのタスクがあります！'
+    end
   end
 
   private
